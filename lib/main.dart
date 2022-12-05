0eed4dafb8537a504cadd7a03105a18dde965d73
thefarismd <thefarismd@icloud.com>
2022-12-05 11:43:46 +0800

    master

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PropertyApp());
+}
+
+class PropertyApp extends StatelessWidget {
+  // const MyWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
